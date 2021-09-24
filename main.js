9baf0dff0d70cd133b49d4b4fc13e16999e3c72d
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-09-23 20:42:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function soundClassify(){
+    navigator.mediaDevices.getUserMedia({
+       audio:true 
+    });
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/ebdXozvWY/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        
+ }
+ }
+ 
